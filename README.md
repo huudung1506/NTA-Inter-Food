a9ba7af116e177580d23003b855df83dd8018044
HuuDung15 <nhuudung10@gmail.com>
2022-07-05 12:09:03 +0700

    update

---
+++ README.md
@@ -0,0 +1,8 @@
+# Intern-NTA-OderFood
+Xây dựng website order food and drink
+gồm các chức năng:
+- Đăng nhập, Đăng ký
+- Xem chi tiết món
+- Đặt món
+- Thanh toán
+- Và đang hoàn thiện thêm vài chức năng cơ bản khác...
---
+++ history.php
@@ -0,0 +1,29 @@
+<?php
+require_once('database/dbhelper.php');
+require_once('utils/utility.php');
+
+// $order_id = $order_details_List['order_id'];
+// $product_id = $order_details_List['product_id'];
+// $num = $order_details_List['num'];
+// $price = $order_details_List['price'];
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <!-- Popper JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <!-- <link rel="stylesheet" href="css/index.css"> -->
+    <link rel="stylesheet" href="plugin/fontawesome/css/all.css">
+    <link rel="stylesheet" href="css/cart.css">
+    <title>Giỏ hàng</title>
+</head>
+
